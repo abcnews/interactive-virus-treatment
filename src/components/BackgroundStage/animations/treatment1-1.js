b69296baee03e9e3dd2db7f4eaaadd18c71e3106
Joshua Byrd <phocks@gmail.com>
2020-07-20 11:34:38 +1000

    Update animations

--- src/components/BackgroundStage/animations/treatment1-1.js
+++ src/components/BackgroundStage/animations/treatment1-1.js
@@ -1,33 +1,4 @@
-const animate = ks => {
-  const tl = ks
-    .animate(
-      "#Coronavirus",
-      [
-        {
-          p: "rotate",
-          t: [0, 1000, 2000, 3000],
-          v: [-2, 0, -2, 0],
-          e: [
-            [1, 0.4, 0, 0.6, 1],
-            [1, 0.4, 0, 0.6, 1],
-            [1, 0.4, 0, 0.6, 1],
-            [0]
-          ]
-        }
-      ],
-      "#Coronavirus-label",
-      [{ p: "opacity", t: [500, 1000], v: [0, 1], e: [[0], [0]] }],
-      { autoremove: false, markers: { LoopStart: 1000 } }
-    )
-    .range(0, 3000);
-  if (
-    document.location.search
-      .substr(1)
-      .split("&")
-      .indexOf("global=paused") >= 0
-  )
-    ks.globalPause();
-  return tl;
-};
-
-module.exports = animate;
+const animate = ks => { const tl = ks.animate("#Coronavirus",[{p:'rotate',t:[0,1000,2000,3000],v:[-2,0,-2,0],e:[[1,.4,0,.6,1],[1,.4,0,.6,1],[1,.4,0,.6,1],[0]]}],
+"#Coronavirus-label",[{p:'opacity',t:[500,1000],v:[0,1],e:[[0],[0]]}],
+{autoremove:false,markers:{"LoopStart":1000}}).range(0,3000);
+if(document.location.search.substr(1).split('&').indexOf('global=paused')>=0)ks.globalPause() ; return tl; }; module.exports = animate;
